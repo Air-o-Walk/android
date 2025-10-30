078514623e8a4de8337be67986fc5b256291ed67
Saac04 <santiagoaaguirrec@gmail.com>
2025-10-30 15:59:08 +0100

    WIP adapatar codigo sprint0
    Estoy intentando adaptar mi codigo de sprint0 que es solo un main activity a un fragment. pero lo dejare para despues

--- AiroWalk/app/src/main/java/com/example/air_o_walk/ui/home/HomeFragment.java
+++ AiroWalk/app/src/main/java/com/example/air_o_walk/ui/home/HomeFragment.java
@@ -1,20 +1,27 @@
 package com.example.air_o_walk.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.air_o_walk.databinding.FragmentHomeBinding;
 
+import java.util.Random;
+
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private int numeroRandom;
+
+    private TextView minor;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,10 +31,20 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //final TextView textView = binding.textHome;
+        //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        minor = binding.textMinor;
         return root;
+
+
     }
+
+    public void botonBuscarDispositivosBTLEPulsado( View v ) {
+        numeroRandom = 0;
+        minor.setText(numeroRandom);
+        numeroRandom++;
+    } // ()
 
     @Override
     public void onDestroyView() {
