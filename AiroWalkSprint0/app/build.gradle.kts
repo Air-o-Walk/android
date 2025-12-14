2521516c96e0505594ebb54943f0c1af81bdd308
Meryame Ait Boumlik <meryemboumlik612@gmail.com>
2025-12-14 15:36:32 +0100

    menu done

--- AiroWalkSprint0/app/build.gradle.kts
+++ AiroWalkSprint0/app/build.gradle.kts
@@ -43,4 +43,6 @@ dependencies {
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.drawerlayout:drawerlayout:1.2.0")
+    implementation("com.google.android.material:material:1.11.0")
 }
